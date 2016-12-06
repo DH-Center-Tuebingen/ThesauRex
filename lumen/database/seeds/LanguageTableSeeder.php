80e47c9a0cb7dd1d76bb17eae6f1b4e828bfd96d
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2016-12-06 18:10:25 +0100

    I've changed the short_name from en to gb for English in order to display the right flag within the dropdown.

--- lumen/database/seeds/LanguageTableSeeder.php
+++ lumen/database/seeds/LanguageTableSeeder.php
@@ -20,7 +20,7 @@ class LanguageTableSeeder extends Seeder
         DB::table('th_language')->insert(array(
             'lasteditor'    => 'postgres',
             'display_name'  => 'English',
-            'short_name'    => 'en'
+            'short_name'    => 'gb'
         ));
         DB::table('th_language')->insert(array(
            'lasteditor'    => 'postgres',
