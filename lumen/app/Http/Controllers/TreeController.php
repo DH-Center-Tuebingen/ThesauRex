44354846af7572bb7a3396b1bbda8f18ce7d3f5b
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2016-12-20 15:42:35 +0100

    get labels for selected language and order by label instead of url

--- lumen/app/Http/Controllers/TreeController.php
+++ lumen/app/Http/Controllers/TreeController.php
@@ -130,7 +130,7 @@ class TreeController extends BaseController
                 )
             SELECT  q.*
             FROM    q
-            ORDER BY concept_url ASC
+            ORDER BY label ASC
         ");
 
         $concepts = array();
@@ -186,7 +186,11 @@ class TreeController extends BaseController
         $narrower = DB::table($thConcept . ' as c')
             ->join($labelView . ' as f', 'c.concept_url', '=', 'f.concept_url')
             ->join($thBroader .' as broad', 'c.id', '=', 'broad.narrower_id')
-            ->where('broad.broader_id', '=', $id)
+            ->where([
+                [ 'broad.broader_id', '=', $id ],
+                [ 'lang', '=', $lang ]
+            ])
+            ->orderBy('label', 'asc')
             ->get();
         // broader
         $broaderIds = DB::table($thConcept . ' as c')
