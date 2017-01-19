10d1c751b3fa0cc6f02f215c85cd77b3c4a42129
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-01-19 12:53:46 +0100

    remove legacy import/export code

--- export-rdf-from-js.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data = $_POST['data'];
-    $locale = 'de_DE.UTF-8';
-    setlocale(LC_ALL, $locale);
-    putenv('LC_ALL='.$locale);
-    $ret = shell_exec("echo '" . $data . "' | ./3rdparty/scripts/js2skos.py");
-    echo $ret;
-?>
