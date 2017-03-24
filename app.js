ff52658dc7f8c3bbf9e46ac02af3fbf5404bcc10
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-03-24 11:51:51 +0100

    add basic user management support to app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var thesaurexApp = angular.module('tutorialApp', ['ngAnimate', 'ngRoute', 'ngMessages', 'nemLogging', 'ui.select', 'ngSanitize', 'ngFlag', 'ui.bootstrap', 'ngFileUpload', 'ui.tree', 'ui.bootstrap.contextMenu']);
+var thesaurexApp = angular.module('tutorialApp', ['ngAnimate', 'satellizer', 'ui.router', 'ngRoute', 'ngMessages', 'nemLogging', 'ui.select', 'ngSanitize', 'ngFlag', 'ui.bootstrap', 'ngFileUpload', 'ui.tree', 'ui.bootstrap.contextMenu']);
 
 thesaurexApp.directive('spinner', function() {
     return {
@@ -52,7 +52,9 @@ thesaurexApp.directive('resizeWatcher', function($window) {
             var width = $window.innerWidth;
             var isSm = window.matchMedia("(max-width: 991px)").matches;
             var controlElement = $('.control-elements')[0];
-            var controlHeight = controlElement.offsetHeight + controlElement.offsetTop;
+            var controlHeight;
+            if(controlElement) controlHeight = controlElement.offsetHeight + controlElement.offsetTop;
+            else controlHeight = 0;
             if(isSm) {
                 $('#master-tree').css('height', '');
                 $('#project-tree').css('height', '');
@@ -62,12 +64,17 @@ thesaurexApp.directive('resizeWatcher', function($window) {
                 $('#alternative-list').css('height', '');
             } else {
                 var headerHeight = document.getElementById('main-container').offsetTop;
-                var informationHeight = document.getElementById('information-header').offsetHeight;
-                var informationAlertHeight = document.getElementById('information-alert').offsetHeight;
+                var informationHeight = 0;
+                var informationHeader = document.getElementById('information-header');
+                if(informationHeader) informationHeight = informationHeader.offsetHeight;
+                var informationAlertHeight = 0;
+                var informationAlert = document.getElementById('information-alert');
+                if(informationAlert) informationAlertHeight = informationAlert.offsetHeight;
                 informationHeight += informationAlertHeight;
                 //use broader-header height as subHeaderHeight, since all *-header should have the same height
                 var subHeader = document.getElementById('broader-header');
-                var subHeaderHeight = subHeader.offsetHeight + subHeader.offsetTop;
+                var subHeaderHeight = 0;
+                if(subHeader) subHeaderHeight = subHeader.offsetHeight + subHeader.offsetTop;
 
                 var containerHeight = height - controlHeight - headerHeight - bottomPadding;
                 var rightHeight = height - headerHeight - (bottomPadding - (listPadding/4)) - informationHeight - listPadding;
@@ -230,24 +237,116 @@ thesaurexApp.factory('httpGetFactory', function($http) {
     };
 });
 
-thesaurexApp.factory('scopeService', function($http) {
-    var service = {
-    };
-    return service;
-});
+thesaurexApp.config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide) {
+    var lastError;
+    var rejectReasons = [
+        'user_not_found',
+        'token_not_provided',
+        'token_expired',
+        'token_absent',
+        'token_invalid'
+    ];
+    var rejectTranslationKeys = [
+        "login.error.user-not-found",
+        "login.error.token-not-provided",
+        "login.error.token-expired",
+        "login.error.token-absent",
+        "login.error.token-invalid"
+    ];
 
-thesaurexApp.config(function($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/tree', {
+    function updateToken(response, $injector) {
+        if(response.headers('Authorization') !== null) {
+            var token = response.headers('Authorization').replace('Bearer ', '');
+            var $auth = $injector.get('$auth');
+            $auth.setToken(token);
+            localStorage.setItem('satellizer_token', token);
+        }
+    }
+
+    function setAuthHeader($q, $injector) {
+        return {
+            response: function(response) {
+                updateToken(response, $injector);
+                return response || $q.when(response);
+            },
+            responseError: function(rejection) {
+                console.log("Something went wrong...");
+                var reasonIndex = rejectReasons.indexOf(rejection.data.error);
+                if(rejection.data && reasonIndex > -1) {
+                    localStorage.removeItem('user');
+                    var userService = $injector.get('userService');
+                    userService.loginError.message = rejectTranslationKeys[reasonIndex];
+                } else if(rejection.status == 400 || rejection.status == 401) {
+                    var $state = $injector.get('$state');
+                    var userService = $injector.get('userService');
+                    userService.loginError.message = 'login.error.400-or-401';
+                    localStorage.removeItem('user');
+                    $state.go('auth');
+                } else {
+                    updateToken(rejection, $injector);
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(rejection.data, "text/xml");
+                    var errors = doc.getElementsByClassName('exception_message');
+                    var modalFactory = $injector.get('modalFactory');
+                    var errorMsg;
+                    if(typeof errors[0] != 'undefined' && errors[0].innerHTML) {
+                        errorMsg = errors[0].innerHTML;
+                    } else {
+                        errorMsg = rejection.statusText;
+                    }
+                    if(!lastError || lastError != rejection.status) {
+                        lastError = rejection.status;
+                        modalFactory.errorModal(errorMsg);
+                    }
+                }
+                return $q.reject(rejection);
+            }
+        };
+    }
+    $provide.factory('setAuthHeader', setAuthHeader);
+
+    $httpProvider.interceptors.push('setAuthHeader');
+
+	$authProvider.baseUrl = '.';
+    $authProvider.loginUrl = 'api/user/login';
+    $urlRouterProvider.otherwise('/auth');
+
+    $stateProvider
+        .state('auth', {
+            url: '/auth',
+            templateUrl: 'layouts/login.html',
+            controller: 'userCtrl'
+        })
+        .state('thesaurex', {
+            url: '/thesaurex',
             templateUrl: 'includes/tree.html'
         })
-        /*.when('/user/role/:activeRole', {
-            template: '',
-            controller: 'headerCtrl'
-        })*/
-        .otherwise({
-            redirectTo: '/tree'
+        .state('user', {
+            url: '/user',
+            templateUrl: 'user.html'
+        })
+        .state('roles', {
+            url: '/roles',
+            templateUrl: 'roles.html'
         });
+});
 
-    //$locationProvider.html5Mode(true);
+/**
+ * Redirect user to 'spacialist' state if they are already logged in and access the 'auth' state
+ */
+thesaurexApp.run(function($rootScope, $state, userService) {
+    $rootScope.$on('$stateChangeStart', function(event, toState) {
+        var user = localStorage.getItem('user');
+        if(user !== '') {
+            parsedUser = JSON.parse(user);
+            if(parsedUser) {
+                userService.currentUser.user = parsedUser.user;
+                userService.currentUser.permissions = parsedUser.permissions;
+                if(toState.name === 'auth') {
+                    event.preventDefault();
+                    $state.go('thesaurex');
+                }
+            }
+        }
+    });
 });
