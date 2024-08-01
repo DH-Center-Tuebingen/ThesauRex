8a4fdc3fb1e57b22527e097731e6b53a36817cc3
Severino <severin.opel@hotmail.com>
2024-08-01 13:22:11 +0200

    Login extracted to dhc-components

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -10,23 +10,12 @@
                 width="128"
             />
             <h1 class="mt-4 fw-bold">ThesauRex</h1>
-
-            <Login 
-                :loading="state.loading"
-                :disabled="state.disabled"
-                v-model:username="state.user.email"
-                v-model:password="state.user.password"
-                @login="login"
-                :error="state.error"
-                :errorMessage="state.errorMessage"
-                :passwordLabel="{text: 'Password', icon: 'fas fa-lock'}"
-                :userLabel="{text: 'Email or Nickname', icon: 'fas fa-lock'}"
-                :submitText="'Login'"
-                :stayLoggedIn="{text: 'Stay logged in', }"
-
-            />
         </div>
-      
+        <Login
+            :loading="state.loading"
+            @login="login"
+            :error="state.errorMessage"
+        />
     </div>
 </template>
 
@@ -38,7 +27,7 @@
         onMounted,
     } from 'vue';
 
-    import Login from 'dhc-components/Login'
+    import { Login } from 'dhc-components'
 
     import { useI18n } from 'vue-i18n';
     import { useRoute } from 'vue-router';
@@ -48,7 +37,6 @@
     import {
         initApp,
         getErrorMessages,
-        getValidClass
     } from '@/helpers/helpers.js';
 
     // import Alert from './Alert.vue';
@@ -70,64 +58,55 @@
                 redirect: {
                     name: 'home'
                 },
-                error: false,
-                errorMessage: {
-                    type: '',
-                    message: '',
-                }
+                errorMessage: '',
             });
 
             // FUNCTIONS
-            const login = async _ => {
+            const login = async args => {
                 state.loading = true;
-                state.error = false;
 
                 let data = {
-                    password: state.user.password
+                    password: args.password
                 };
                 // dirty check if email field should be treated
                 // as actual email address or nickname
-                if (state.user.email.includes('@')) {
-                    data.email = state.user.email;
+                if(args.username.includes('@')) {
+                    data.email = args.username;
                 } else {
-                    data.nickname = state.user.email;
+                    data.nickname = args.username;
                 }
 
                 try {
                     await auth.login({
-                        data: data,
-                        staySignedIn: state.user.remember,
+                        data,
+                        staySignedIn: args.staySignedIn,
                         redirect: state.redirect,
                         fetchUser: true
                     })
                     initApp(locale)
-                } catch (e) {
-                    state.error = true;
+                } catch(e) {
                     const errorObject = getErrorMessages(e);
-                    state.errorMessage = {
-                        type: 'error',
-                        text: Object.values(errorObject).join(' ::: ')
-                    }; 
+                    state.errorMessage = Object.values(errorObject).join(' ::: ');
                 }
                 state.loading = false;
             };
 
             // ON MOUNTED
             onMounted(_ => {
-                if (auth.check()) {
+                if(auth.check()) {
                     router.push({
                         name: 'home'
                     });
                 }
                 const lastRoute = auth.redirect() ? auth.redirect().from : undefined;
                 const currentRoute = useRoute();
-                if (lastRoute && lastRoute.name != 'login') {
+                if(lastRoute && lastRoute.name != 'login') {
                     state.redirect = {
                         name: lastRoute.name,
                         params: lastRoute.params,
                         query: lastRoute.query
                     };
-                } else if (currentRoute.query && currentRoute.query.redirect) {
+                } else if(currentRoute.query && currentRoute.query.redirect) {
                     state.redirect = {
                         path: currentRoute.query.redirect
                     };
@@ -139,7 +118,6 @@
                 t,
                 state,
                 login,
-                getValidClass,
             };
         },
     }
