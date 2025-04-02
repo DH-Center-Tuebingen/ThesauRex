932c0dc02a2f283578eaaeeaa1fb657e5f4db24c
Severino <severin.opel@hotmail.com>
2025-04-02 13:35:21 +0200

    Adjusted vite to preven warnings

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,11 @@ export default ({mode}) => {
     verifyEnvVars(env);
 
     const config = {
+        define: {
+            __VUE_I18N_FULL_INSTALL__: true,
+            __VUE_I18N_LEGACY_API__: false,
+            __INTLIFY_PROD_DEVTOOLS__: false,
+        },
         plugins: [
             laravel({
                 input: [
