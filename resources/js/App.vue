18b04af49c6d78898feb55ba3f76ac40a69c9011
Severino <severin.opel@hotmail.com>
2025-04-02 13:36:07 +0200

    Removed v-html

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -102,7 +102,7 @@
                     <div>
                         <i class="fas fa-5x fa-fw fa-spinner fa-spin"></i>
                     </div>
-                    <h1 class="mt-5" v-html="t('app.loading_screen_msg', {appname: state.appName})"></h1>
+                    <h1 class="mt-5"><span class="fw-light">{{t('app.loading_screen_msg')}}</span>&nbsp;{{state.appName}}&nbsp;<span class="fw-light">&hellip;</span></h1>
                 </div>
             </template>
         </div>
