124a802e97d03ca16523c793083084a77ab270fd
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2025-04-30 10:16:02 +0200

    add updating and deleting event handlers

--- app/Http/Controllers/TreeController.php
+++ app/Http/Controllers/TreeController.php
@@ -776,9 +776,14 @@ class TreeController extends Controller
                 break;
             case 'level':
                 if($concept->is_top_concept) {
-                    th_tree_builder($treeName)
+                    $narrowersAsTopConcepts = th_tree_builder($treeName)
                         ->whereIn('id', $narrowers)
-                        ->update(['is_top_concept' => true]);
+                        ->whereNot('is_top_concept')
+                        ->get();
+                    foreach($narrowersAsTopConcepts as $updateConcept) {
+                        $updateConcept->is_top_concept = true;
+                        $updateConcept->save();
+                    }
                 }
                 foreach($broaders as $broaderId) {
                     foreach($narrowers as $narrowerId) {
@@ -801,9 +806,14 @@ class TreeController extends Controller
                 }
                 break;
             case 'top':
-                th_tree_builder($treeName)
+                $narrowersAsTopConcepts = th_tree_builder($treeName)
                     ->whereIn('id', $narrowers)
-                    ->update(['is_top_concept' => true]);
+                    ->whereNot('is_top_concept')
+                    ->get();
+                foreach($narrowersAsTopConcepts as $updateConcept) {
+                    $updateConcept->is_top_concept = true;
+                    $updateConcept->save();
+                }
                 break;
             case 'rerelate':
                 $newParentId = $request->query('p');
