dd01f24ddf73c0dc62bca30c37970e233e714416
Severino <severin.opel@hotmail.com>
2025-05-06 13:55:17 +0200

    Removed comments

--- resources/js/components/tree/Node.vue
+++ resources/js/components/tree/Node.vue
@@ -229,31 +229,5 @@
                 state,
             };
         },
-        // methods: {
-        //     onDragEnter() {
-        //         if(!this.data.dragAllowed()) return;
-        //         this.asyncToggle.clear();
-        //         this.asyncToggle();
-        //     },
-        //     onDragLeave(item) {
-        //     },
-        //     doToggle() {
-        //         if(!this.data.state.opened && this.data.state.openable) {
-        //             this.data.onToggle({data: this.data});
-        //         }
-        //     }
-        // },
-        // data() {
-        //     return {
-        //     }
-        // },
-        // computed: {
-        //     asyncToggle() {
-        //         return _debounce(this.doToggle, this.data.dragDelay || 500);
-        //     },
-        //     label() {
-        //         return this.$getLabel(this.data);
-        //     }
-        // }
     }
 </script>
